161c4916ba73380d12520b2d83b3004379ecbf10
Stephen Kwok <schunman@gmail.com>
2025-01-28 20:57:25 +0800

    Services commit

---
+++ common-lib/src/test/java/com/example/common_lib/CommonLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.common_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
